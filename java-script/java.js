7bd4e30996ed5fb857b875a36d6a3ad6ac8369d0
Arik Smorodinsky <Arikserg1965@gmail.com>
2019-06-11 15:30:18 +0300

    added new file

--- java-script/java.js
+++ java-script/java.js
@@ -3,3 +3,7 @@ if(5>2) check==true
 
 boolean password = check&&false   // notice the space we're leaving at the end
 console.log(password) // logs "The potential of the future is the best thing ever"
+console.log("hey")
+let a=4
+let b=3
+
