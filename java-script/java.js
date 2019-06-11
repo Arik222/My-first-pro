86fb795c8a6879ed0b7218468bc817957e2a8797
Arik Smorodinsky <Arikserg1965@gmail.com>
2019-06-11 14:57:59 +0300

    first commit

---
+++ java-script/java.js
@@ -0,0 +1,5 @@
+booelan check=false
+if(5>2) check==true
+
+boolean password = check&&false   // notice the space we're leaving at the end
+console.log(password) // logs "The potential of the future is the best thing ever"
